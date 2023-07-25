188a5b91e0f5c1f9b3d62df4e4b68a689d80cb77
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-07-25 15:09:55 +0300

    Добавьте файлы проекта.

---
+++ Square into Squares. Protect trees!/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Decompose
+{
+    public static void Main()
+    {
+        Decompose dc = new();
+        // Test
+        var t = dc.decompose(11);
+        // ...should return "1 2 4 10"
+    }
+
+    public string? decompose(long n)
+    {
+        long remainder = 0;
+        List<long> indexes = new() { n };
+
+        while (true)
+        {
+            if (indexes.Count == 0)
+                return null;
+
+            long curInd = indexes[^1];
+            indexes.Remove(indexes[^1]);
+            remainder += curInd * curInd;
+
+            for (long i = curInd - 1; i > 0; i--)
+            {
+                if (remainder - i * i >= 0)
+                {
+                    remainder -= i * i;
+                    indexes.Add(i);
+
+                    if (remainder == 0)
+                        return string.Join(" ", indexes.OrderBy(i => i).Select(i => i.ToString()));
+                }
+            }
+        }
+    }
+}
